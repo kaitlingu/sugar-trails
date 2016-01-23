59141b880cfeec5a8a9901a2704395056aee8a77
Jessica Joseph <Jessicajoseph@nyu.edu>
2016-01-23 08:55:07 -0500

    Server is working

--- Podfile
+++ Podfile
@@ -4,6 +4,8 @@
 target 'SugarTrails' do
 
 pod 'Alamofire', '3.1.4'
+pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
+
 
 end
 
--- SugarTrails/FoodRec.swift
+++ SugarTrails/FoodRec.swift
@@ -9,13 +9,22 @@
 import UIKit
 import Alamofire
 
-class FoodRec: UIViewController{
+class FoodRec: UIViewController, UITextFieldDelegate{
     
+    @IBAction func submit(sender: AnyObject) {
+    }
     let serverURL = "https://jnj-dev.apigee.net/otr/v1/patient/-/healthdata/search?type=bloodGlucose,bolusInsulin,exercise,food&startDate=2016-01-03T00%3A00%3A00&endDate=2016-01-23T03%3A46%3A48&limit=5000&offset=0"
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        username.delegate = self
+        password.delegate = self
+        
         
         let headers = [
             "Authorization": "Bearer 7obv5AlUXBlrAz8VnHv7vhIr16SG"
@@ -24,13 +33,9 @@ class FoodRec: UIViewController{
         //Server Request
         Alamofire.request(.GET, serverURL, headers:headers)
             .responseJSON { response in
-                print(response.request)  // original URL request
-                print(response.response) // URL response
-                print(response.data)     // server data
-                print(response.result)   // result of response serialization
-                
+
                 if let JSON = response.result.value {
-                    print("JSON: \(JSON)")
+                    print("JSON: \n \(JSON)")
                 }
         }
         
@@ -42,5 +47,11 @@ class FoodRec: UIViewController{
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        textField.resignFirstResponder()
+        return true;
+    }
     
 }
