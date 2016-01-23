d76eec5b1ed7126b33f0b431f4f372d620410539
kaitlingu <kaitlin.gu@gmail.com>
2016-01-23 00:44:55 -0500

    require

--- sugartrails/routes/users.js
+++ sugartrails/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/data', function(req, res, next) {
+  // get request on route data
+  // require of json file
+  // res.send(// json file)
+});
+
 module.exports = router;
