5f203643973a669615033d13c9efb8b12cd1aaf3
kaitlingu <kaitlin.gu@gmail.com>
2016-01-23 02:31:41 -0500

    whoops

--- sugartrails/routes/index.js
+++ sugartrails/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sugar Trails' });
+  res.render('index', { title: 'Sugar Trails', main:'app'});
+});
+
+router.get('/app', function(req, res, next) {
+  res.render('app');
 });
 
 module.exports = router;
