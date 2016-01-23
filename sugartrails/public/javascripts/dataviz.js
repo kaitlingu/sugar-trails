7349ef226a2dba6f7a9222aab00ee5d495528dcc
kaitlingu <kaitlin.gu@gmail.com>
2016-01-23 11:41:29 -0500

    augh

--- sugartrails/public/javascripts/dataviz.js
+++ sugartrails/public/javascripts/dataviz.js
@@ -28,6 +28,96 @@ var path = svg.selectAll("path")
     .attr("d", arc);
 */
 
+var dataset = [];
+
+ var w = 8000;
+			var h = 200;
+
+ $.ajax({
+  url:"https://jnj-dev.apigee.net/otr/v1/patient/-/healthdata/search?type=bloodGlucose,bolusInsulin,exercise,food&startDate=2016-01-01T00%3A00%3A00&endDate=2016-02-22T00%3A00%3A00&limit=5000&offset=0",
+  beforeSend: function(xhr) { xhr.setRequestHeader('authorization', 'Bearer 0S6SM4v0ioevB4DcB6kGclazPAbh'); },
+  success: function (response) {
+  	for (var i = 0; i < response['bloodGlucose'].length; i++){
+  		dataset.push([i*10, response['bloodGlucose'][i]['bgValue']['value']]);
+   // console.log(i, response['bloodGlucose'][i]['bgValue']['value']);
+}
+var svg = d3.select("#graph")
+
+						.append("svg")
+						.attr("width", 1000)
+						.attr("height", 300);
+
+console.log(dataset);
+
+
+svg.selectAll("circle")
+			   .data(dataset)
+			   .enter()
+
+			   .append("circle")
+			   .attr('fill-opacity', 0.2)
+			.attr('fill', 'red')
+			   .attr("cx", function(d) {
+			   		return d[0];
+			   })
+			   .attr("cy", function(d) {
+			   		return d[1];
+			   })
+			   .attr("r", function(d) {
+			   		
+			   		return Math.sqrt(h - d[1]);
+			   	
+			   });
+
+
+			   svg.selectAll('circle').on("mouseover", rollover);
+			svg.selectAll('circle').on("mouseleave",rollaway)
+
+var details = d3.select("#info")
+						.attr("width", w)
+						.attr("height", h);
+					
+			function rollover(){
+					console.log(this);
+					d3.select(this).attr('fill-opacity',1.0)
+								.attr("stroke-width",3.0)
+								.attr("stroke", "black");
+
+					console.log(this);}
+
+
+
+			function rollaway(){
+				/*details.selectAll("foreignObject").remove()
+				svg.selectAll("rect.negative").remove()*/
+
+												d3.select(this).attr('fill-opacity',0.2)
+												.attr("stroke","none");
+
+
+
+				console.log(this);
+			}
+  },
+  error: function (xhr, status) {
+    //handle errors
+  }
+});
+
+
+			
+		
+			//Create SVG element
+			
+			
+
+			
+
+			
+
+
+/*
+
 
 (function(d3) {
         'use strict';
@@ -107,7 +197,7 @@ var path = svg.selectAll("path")
             tooltip.style('top', (d3.event.pageY + 10) + 'px')
               .style('left', (d3.event.pageX + 10) + 'px');
           });
-          */
+          
             
           var legend = svg.selectAll('.legend')
             .data(color.domain())
@@ -169,54 +259,6 @@ var path = svg.selectAll("path")
 
       })(window.d3);
 
-var w = 500;
-			var h = 200;
-			
-			var dataset = [
-							[4, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-							[410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-						  ];
-	
-			//Create SVG element
-			var svg = d3.select("#graph")
-
-						.append("svg")
-						.attr("width", w)
-						.attr("height", h);
-
-			svg.selectAll("circle")
-			   .data(dataset)
-			   .enter()
-
-			   .append("circle")
-			   .attr('fill-opacity', 0.2)
-			.attr('fill', 'red')
-			   .attr("cx", function(d) {
-			   		return d[0];
-			   })
-			   .attr("cy", function(d) {
-			   		return d[1];
-			   })
-			   .attr("r", function(d) {
-			   		return Math.sqrt(h - d[1]);
-			   });
-
-			
-
-			svg.selectAll('circle').on("mouseover", rollover);
-			svg.selectAll('circle').on("mouseleave",rollaway)
-
-var details = d3.select("#info")
-						.attr("width", w)
-						.attr("height", h);
-					
-			function rollover(){
-					console.log(this);
-					d3.select(this).attr('fill-opacity',1.0)
-								.attr("stroke-width",3.0)
-								.attr("stroke", "black");
-
-					console.log(this);
 
 					
 					/*details.append("foreignObject")
@@ -258,16 +300,4 @@ svg.append("foreignObject")
 
 
 								// i hate these people 
-			}
-
-			function rollaway(){
-				/*details.selectAll("foreignObject").remove()
-				svg.selectAll("rect.negative").remove()*/
-
-												d3.select(this).attr('fill-opacity',0.2)
-												.attr("stroke","none");
-
-
-
-				console.log(this);
-			}
+			
--- sugartrails/routes/index.js
+++ sugartrails/routes/index.js
@@ -2,12 +2,33 @@ var request = require('request');
 var express = require('express');
 var router = express.Router();
 
+var options = {
+  url: 'hhttps://jnj-dev.apigee.net/otr/v1/patient/-/healthdata/search?type=bloodGlucose,bolusInsulin,exercise,food&startDate=2016-01-01T00%3A00%3A00&endDate=2016-02-22T00%3A00%3A00&limit=5000&offset=0',
+  headers: {
+    'Accept': 'application/json',
+    'Authorization': 'Bearer 0S6SM4v0ioevB4DcB6kGclazPAbh'
+  }
+};
+
+var info;
+function callback(error, response, body) {
+
+  if (!error && response.statusCode == 200) {
+    info = JSON.parse(body);
+    console.log(info);
+  }
+}
+
+request(options, callback);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sugar Trails', main:'app'});
 });
 
 router.get('/app', function(req, res, next) {
+	    console.log(info);
+
   res.render('app');
 });
 
